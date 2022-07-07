cc8a76cefddac9ed4309426c75d4a3bdcf2383cc
cleandevcode <52365167+cleandevcode@users.noreply.github.com>
2022-07-08 02:40:42 +0800

    okta integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { OKTA_CONFIG, OktaAuthModule } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+const oktaDomain = 'oktadev.cooperators.ca';
+const config = {
+  issuer: `https://${oktaDomain}/oauth2/default`,
+  clientId: '0oa17p1e4gmCiudNH0h8',
+  redirectUri: window.location.origin + '/sign-in/callback',
+};
+
+const oktaAuth = new OktaAuth(config);
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    OktaAuthModule,
+  ],
+  providers: [
+    {
+      provide: OKTA_CONFIG,
+      useValue: { oktaAuth },
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { OktaAuthStateService, OKTA_AUTH } from '@okta/okta-angular';
+import { AuthState, OktaAuth } from '@okta/okta-auth-js';
+import { Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.css'],
+  styleUrls: ['./signin.component.scss'],
 })
 export class SignInComponent implements OnInit {
   text = 'Insurance\n and Investment';
@@ -16,11 +20,25 @@ export class SignInComponent implements OnInit {
   email = '';
   password = '';
   recoveredEmail = false;
-  constructor(private fB: FormBuilder, private router: Router) {
+  signInError = false;
+  loading = false;
+
+  public isAuthenticated$!: Observable<boolean>;
+
+  constructor(
+    private fB: FormBuilder,
+    private router: Router,
+    private _oktaStateService: OktaAuthStateService,
+    @Inject(OKTA_AUTH) private _oktaAuth: OktaAuth
+  ) {
     if (this.router.getCurrentNavigation()?.extras.state) {
       this.recoveredEmail = true;
       //@ts-ignore
       this.email = this.router.getCurrentNavigation()?.extras.state?.email;
+      this.isAuthenticated$ = this._oktaStateService.authState$.pipe(
+        filter((s: AuthState) => !!s),
+        map((s: AuthState) => s.isAuthenticated ?? false)
+      );
     }
   }
 
@@ -40,5 +58,26 @@ export class SignInComponent implements OnInit {
     this.submitted = true;
 
     if (this.form.invalid) return;
+  }
+
+  public async login(): Promise<void> {
+    this.signInError = false;
+    if (this.email && this.password) {
+      this.loading = true;
+      await this._oktaAuth
+        .signInWithCredentials({
+          username: this.email,
+          password: this.password,
+        })
+        .then((_) => {
+          console.log('login success!!!');
+          // this.router.navigate(['/dashboard'])
+        })
+        .catch((err) => {
+          console.log('error>>>', err);
+          this.signInError = true;
+        })
+        .finally(() => (this.loading = false));
+    }
   }
 }
